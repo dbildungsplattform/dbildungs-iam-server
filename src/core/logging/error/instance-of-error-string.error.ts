ac63d1c28c3ba1e079b5cc9e098a8f30b83db280
Thomas Feldtkeller <thomas.feldtkeller@dataport.de>
2026-09-07 14:45:56 +0200

    refactor class logger errors

---
+++ src/core/logging/error/instance-of-error-string.error.ts
@@ -0,0 +1,7 @@
+import { DomainError } from "../../../shared/error/domain.error.js";
+
+export class InstanceOfErrorStringError extends DomainError {
+    public constructor(details?: unknown[] | Record<string, undefined>) {
+        super('Type of parameter was String when calling instanceOfError, that may not have been intentional',  'INSTANCE_OF_ERROR_STRING', details);
+    }
+}
