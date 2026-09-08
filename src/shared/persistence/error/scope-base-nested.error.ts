3fde6ca85b878093cd210fd660ab6be48a911a54
Thomas Feldtkeller <thomas.feldtkeller@dataport.de>
2026-09-08 11:40:43 +0200

    refactor scope base error

---
+++ src/shared/persistence/error/scope-base-nested.error.ts
@@ -0,0 +1,7 @@
+import { DomainError } from "../../error/domain.error.js";
+
+export class ScopeBaseNestedError extends DomainError {
+    public constructor(details?: unknown[] | Record<string, unknown>) {
+        super('Scope where operator is already set. Scope Operator can not be nested', 'SCOPE_BASE_NESTED_ERROR', details);
+    }
+}
