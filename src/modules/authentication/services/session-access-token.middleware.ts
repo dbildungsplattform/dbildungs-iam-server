3a5aaeb394bdf8b27d9fdd8f6c55fdfb69b139b9
Marvin Rode (Cap) <127723478+marode-cap@users.noreply.github.com>
2026-09-16 09:54:07 +0200

    Fix token introspection (#1307)

--- src/modules/authentication/services/session-access-token.middleware.ts
+++ src/modules/authentication/services/session-access-token.middleware.ts
@@ -38,11 +38,11 @@ export class SessionAccessTokenMiddleware implements NestMiddleware {
         }
 
         if (accessToken) {
-            const isAccessTokenActive: boolean = (await this.client.introspect(accessToken)).active;
+            const isAccessTokenActive: boolean = (await this.client.introspect(accessToken, 'access_token')).active;
             if (!isAccessTokenActive) {
                 // Do we have a refresh token and somewhere to store the result of the refresh?
                 const isRefreshTokenActive: boolean =
-                    refreshToken !== undefined && (await this.client.introspect(refreshToken)).active;
+                    refreshToken !== undefined && (await this.client.introspect(refreshToken, 'refresh_token')).active;
                 if (refreshToken && isRefreshTokenActive && req.passportUser) {
                     try {
                         const tokens: TokenSet = await this.client.refresh(refreshToken);
