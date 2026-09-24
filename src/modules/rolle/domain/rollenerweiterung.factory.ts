632ec60514205d0e1041c5c1c05a2cfa9bcea6fc
martkrau <martin.krause@capgemini.com>
2026-09-24 10:33:15 +0200

    domain and factory adaption

--- src/modules/rolle/domain/rollenerweiterung.factory.ts
+++ src/modules/rolle/domain/rollenerweiterung.factory.ts
@@ -1,56 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import {
-    OrganisationID,
-    RolleID,
-    RollenerweiterungID,
-    ServiceProviderID,
-} from '../../../shared/types/aggregate-ids.types.js';
-import { OrganisationRepository } from '../../organisation/persistence/organisation.repository.js';
-import { ServiceProviderRepo } from '../../service-provider/repo/service-provider.repo.js';
-import { RolleRepo } from '../repo/rolle.repo.js';
+import { OrganisationID, RollenerweiterungID, ServiceProviderID } from '../../../shared/types/aggregate-ids.types.js';
+import { NoRedundantRollenerweiterungError } from '../specification/error/no-redundant-rollenerweiterung.error.js';
+import { Rolle } from './rolle.js';
 import { Rollenerweiterung } from './rollenerweiterung.js';
 
 @Injectable()
 export class RollenerweiterungFactory {
-    public constructor(
-        private readonly organisationRepo: OrganisationRepository,
-        private readonly rolleRepo: RolleRepo,
-        private readonly serviceProviderRepo: ServiceProviderRepo,
-    ) {}
-
     public construct(
         id: RollenerweiterungID,
         createdAt: Date,
         updatedAt: Date,
         organisationId: OrganisationID,
-        rolleId: RolleID,
+        rolleId: string,
         serviceProviderId: ServiceProviderID,
     ): Rollenerweiterung<true> {
-        return Rollenerweiterung.construct<true>(
-            this.organisationRepo,
-            this.rolleRepo,
-            this.serviceProviderRepo,
-            id,
-            createdAt,
-            updatedAt,
-            organisationId,
-            rolleId,
-            serviceProviderId,
-        );
+        return Rollenerweiterung.construct(id, createdAt, updatedAt, organisationId, rolleId, serviceProviderId);
     }
 
     public createNew(
         organisationId: OrganisationID,
-        rolleId: RolleID,
+        rolle: Rolle<true>,
         serviceProviderId: ServiceProviderID,
-    ): Rollenerweiterung<false> {
-        return Rollenerweiterung.createNew(
-            this.organisationRepo,
-            this.rolleRepo,
-            this.serviceProviderRepo,
-            organisationId,
-            rolleId,
-            serviceProviderId,
-        );
+    ): Result<Rollenerweiterung<false>, NoRedundantRollenerweiterungError> {
+        return Rollenerweiterung.createNew(organisationId, rolle, serviceProviderId);
     }
 }
